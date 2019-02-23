419ddce24d7899b5e437b1e3b323f59e12038dc8
tofer17 <tofer17@gmail.com>
2019-02-23 15:33:27 -0600

    [IC] TBE and TBE.html work full life-cycle!
    Time for cleanup!

--- src/main/java/tofer17/ags/TimeBasedEncrypter.java
+++ src/main/java/tofer17/ags/TimeBasedEncrypter.java
@@ -1,15 +1,13 @@
 package tofer17.ags;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.Key;
 import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -84,11 +82,29 @@ public class TimeBasedEncrypter extends HttpServlet {
 
 	private static final Encoder B64Encoder = Base64.getEncoder();
 
+	public static final int TS_MODE_QUERY = 0;
+
+	public static final int TS_MODE_EMBARGO = 1;
+
+	public static final int TS_MODE_OTHER = -1;
+
 	private String signedTimestampAlgo = null;
 
 	private String signedTimestampFormat = null;
 
+	private String signedTimestampTSFormat = "%1$s:%2$s";
+
 	private String signedTimestampError = null;
+
+	private String embargoKeyError = "{\"error\":-1}";
+
+	private String embargoKeyFormat = "{\"k\":\"%s\"}";
+
+	private String embargoKeyTransformation = "RSA/ECB/PKCS1Padding";
+
+	private int embargoKeySize = 32;
+
+	private String embargoKeyAlgo = "AES";
 
 	private KeyPair keyPair = null;
 
@@ -106,8 +122,15 @@ public class TimeBasedEncrypter extends HttpServlet {
 	static {
 		final Properties p = new Properties();
 		p.setProperty( "tofer17.ags.tbe.signed.timestamp.algo", "SHA256withRSA" );
-		p.setProperty( "tofer17.ags.tbe.signed.timestamp.format", "{\"t\":%1$s,\"s\":\"%2$s\",\"k\":\"%3$s\"}" );
+		p.setProperty( "tofer17.ags.tbe.signed.timestamp.format",
+			"{\"t\":%1$s,\"m\":%2$s,\"s\":\"%3$s\",\"k\":\"%4$s\"}" );
 		p.setProperty( "tofer17.ags.tbe.signed.timestamp.error", "{\"error\":-1}" );
+
+		p.setProperty( "tofer17.ags.tbe.embargo.key.error", "{\"error\":-1}" );
+		p.setProperty( "tofer17.ags.tbe.embargo.key.format", "{\"k\":\"%s\"}" );
+		p.setProperty( "tofer17.ags.tbe.embargo.key.transformation", "RSA/ECB/PKCS1Padding" );
+		p.setProperty( "tofer17.ags.tbe.embargo.key.size", "32" );
+		p.setProperty( "tofer17.ags.tbe.embargo.key.algo", "AES" );
 		// Although a neat idea-- not happening.
 		// p.setProperty( "tofer17.ags.tbe.key.algo", "RSA" );
 		// p.setProperty( "tofer17.ags.tbe.key.size", "2048" );
@@ -213,7 +236,7 @@ public class TimeBasedEncrypter extends HttpServlet {
 			saveConfig();
 		} else if ( saveNeeded ) {
 			logger.warn(
-				"Configuration needs to be saved, however, we cannot since we could neither load nore generate our key-pair" );
+				"Configuration needs to be saved, however, we cannot since we could neither load nor generate our key-pair" );
 		}
 
 	}
@@ -257,63 +280,8 @@ public class TimeBasedEncrypter extends HttpServlet {
 		}
 	}
 
-	private static final KeyPair xloadOrGenerateKeyPair ( File publicKeyFile, File privateKeyFile ) {
-
-		// Check if the files exist
-		if ( !publicKeyFile.canRead() || !privateKeyFile.canRead() ) {
-			logger.info( "Cannot read from '{}' or '{}'-- creating new key pair.", publicKeyFile, privateKeyFile );
-			try {
-
-				final RSAKeyGenParameterSpec params = new RSAKeyGenParameterSpec( 2048, RSAKeyGenParameterSpec.F0 );
-
-				final KeyPairGenerator keyGen = KeyPairGenerator.getInstance( "RSA" );
-
-				keyGen.initialize( params );
-
-				final KeyPair kp = keyGen.generateKeyPair();
-
-				Files.write( Paths.get( publicKeyFile.getAbsolutePath() ), kp.getPublic().getEncoded() );
-				Files.write( Paths.get( privateKeyFile.getAbsolutePath() ), kp.getPrivate().getEncoded() );
-
-				return kp;
-
-			} catch ( NoSuchAlgorithmException e ) {
-				e.printStackTrace();
-			} catch ( InvalidAlgorithmParameterException e ) {
-				e.printStackTrace();
-			} catch ( IOException e ) {
-				e.printStackTrace();
-			}
-		} else { // Load 'em up!
-
-			try {
-				final byte[] encodedPublicKey = Files.readAllBytes( Paths.get( publicKeyFile.getAbsolutePath() ) );
-				final byte[] encodedPrivateKey = Files.readAllBytes( Paths.get( privateKeyFile.getAbsolutePath() ) );
-
-				final X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec( encodedPublicKey );
-				final PKCS8EncodedKeySpec priKeySpec = new PKCS8EncodedKeySpec( encodedPrivateKey );
-
-				final KeyFactory keyFactory = KeyFactory.getInstance( "RSA" );
-
-				final PublicKey publicKey = keyFactory.generatePublic( pubKeySpec );
-				final PrivateKey privateKey = keyFactory.generatePrivate( priKeySpec );
-
-				final KeyPair kp = new KeyPair( publicKey, privateKey );
-
-				logger.info( "Loaded keys from '{}' and '{}'.", publicKeyFile, privateKeyFile );
-
-				return kp;
-			} catch ( NoSuchAlgorithmException e ) {
-				e.printStackTrace();
-			} catch ( InvalidKeySpecException e ) {
-				e.printStackTrace();
-			} catch ( IOException e ) {
-				e.printStackTrace();
-			}
-		}
-
-		return null;
-	}
+// ##############
+// del v  v  v  v
 
 	private CharSequence getKeyFor ( long t ) {
 		try {
@@ -354,8 +322,8 @@ public class TimeBasedEncrypter extends HttpServlet {
 
 			// xform "o" to "oo" as such {"ts":{JSON-TS},"o":"..."}
 			final String oo = new StringBuilder( o.length() + 200 ).append( "{\"t\":" ).append( t )
-				.append( ",\"o\":\"" ).append( o ).append( "\"," ).append( "\"ts\":" ).append( SignedTimestampJSON() )
-				.append( "}" ).toString();
+				.append( ",\"o\":\"" ).append( o ).append( "\"," ).append( "\"ts\":" )
+				.append( getSignedTimestampJSON( TS_MODE_OTHER ) ).append( "}" ).toString();
 
 			logger.info( "oo='{}'", oo );
 
@@ -440,44 +408,7 @@ public class TimeBasedEncrypter extends HttpServlet {
 		return "{\"e\":\"error\"}";
 	}
 
-	public void init ( ServletConfig config ) throws ServletException {
-		// keyPair = loadOrGenerateKeyPair( new File( System.getProperty(
-		// "tofer17.ags.tbe.publicKey" ) ),
-		// new File( System.getProperty( "tofer17.ags.tbe.privateKey" ) ) );
-
-		// log( "Hi!" );
-		super.init( config );
-		loadConfig();
-
-		if ( keyPair == null ) {
-			logger.error( "FATAL: could not load or generate keys!" );
-		}
-
-		// pubKeyExport = new StringBuffer( 400 ).append( "\"k\":\"" )
-		// .append( Base64.getEncoder().encodeToString( keyPair.getPublic().getEncoded()
-		// ) ).append( "\"" ).toString();
-		// pubKeyExport = EncodeToB64( keyPair.getPublic().getEncoded() );
-
-		if ( "1".equals( "2" ) ) { // for debugging, adds r:[...] as part of the pubKeyExp
-			byte[] b = keyPair.getPublic().getEncoded();
-			StringBuffer sb = new StringBuffer().append( "\"r\":[" );
-			for ( int i = 0; i < b.length; i++ ) {
-				sb.append( i > 0 ? "," : "" ).append( b[ i ] );
-			}
-			sb.append( "]," ).append( pubKeyExport );
-			pubKeyExport = sb.toString();
-		}
-
-		logger.info( "TBE initialized {}", SignedTimestampJSON() );
-	}
-
-	@Override
-	public void destroy () {
-		saveConfig();
-		super.destroy();
-	}
-
-	protected void doGet ( HttpServletRequest request, HttpServletResponse response )
+	protected void XdoGet ( HttpServletRequest request, HttpServletResponse response )
 		throws ServletException, IOException {
 		log( "Hi!" );
 		final String t = request.getParameter( "t" );
@@ -485,7 +416,7 @@ public class TimeBasedEncrypter extends HttpServlet {
 		test( System.currentTimeMillis(), "Hello world!" );
 
 		if ( t == null || t.length() < 1 ) {
-			response.getWriter().append( SignedTimestampJSON() );
+			response.getWriter().append( getSignedTimestampJSON( TS_MODE_OTHER ) );
 			return;
 		}
 
@@ -499,7 +430,7 @@ public class TimeBasedEncrypter extends HttpServlet {
 		} else {
 			String o = request.getParameter( "o" );
 			if ( o == null ) {
-				o = SignedTimestampJSON().toString();
+				o = getSignedTimestampJSON( TS_MODE_OTHER ).toString();
 			}
 			s = embargo( d, o );
 		}
@@ -507,12 +438,15 @@ public class TimeBasedEncrypter extends HttpServlet {
 		response.getWriter().append( s );
 	}
 
-	protected void doPost ( HttpServletRequest request, HttpServletResponse response )
+	protected void XdoPost ( HttpServletRequest request, HttpServletResponse response )
 		throws ServletException, IOException {
 		doGet( request, response );
 	}
 
-	private final CharSequence SignedTimestampJSON () {
+// ##############
+// Del ^  ^  ^  ^
+
+	private final CharSequence getSignedTimestampJSON ( int m ) {
 
 		final String timestamp = "" + System.currentTimeMillis();
 
@@ -523,14 +457,154 @@ public class TimeBasedEncrypter extends HttpServlet {
 		try {
 			final Signature sig = Signature.getInstance( signedTimestampAlgo );
 			sig.initSign( keyPair.getPrivate() );
-			sig.update( timestamp.getBytes( StandardCharsets.UTF_8 ) );
+
+			final String dig = String.format( signedTimestampTSFormat, timestamp, m );
+			sig.update( dig.getBytes( StandardCharsets.UTF_8 ) );
+
 			final byte[] sigBytes = sig.sign();
 			final String sigB64 = EncodeToB64( sigBytes );
-			return String.format( signedTimestampFormat, timestamp, sigB64, pubKeyExport );
+
+			return String.format( signedTimestampFormat, timestamp, m, sigB64, pubKeyExport );
 		} catch ( InvalidKeyException | SignatureException | NoSuchAlgorithmException e ) {
 			e.printStackTrace();
 			return signedTimestampError;
 		}
+	}
+
+	private final Key generateKeyForTime ( String t ) throws GeneralSecurityException {
+
+		final Cipher cipher = Cipher.getInstance( embargoKeyTransformation );
+
+		cipher.init( Cipher.ENCRYPT_MODE, keyPair.getPrivate() );
+
+		final byte[] encr = cipher.doFinal( t.getBytes( StandardCharsets.UTF_8 ) );
+
+		final byte[] trunc = new byte[ embargoKeySize ];
+
+		System.arraycopy( encr, 0, trunc, 0, Math.min( trunc.length, encr.length ) );
+
+		return new SecretKeySpec( trunc, embargoKeyAlgo );
+	}
+
+	private final Key getKeyForTime ( String t ) throws GeneralSecurityException {
+
+		return generateKeyForTime( t );
+	}
+
+	private final CharSequence getPastKeyJSON ( String t ) {
+		try {
+			final String keyBase64 = EncodeToB64( getKeyForTime( t ).getEncoded() );
+			return String.format( embargoKeyFormat, keyBase64 );
+		} catch ( GeneralSecurityException e ) {
+			e.printStackTrace();
+		}
+
+		return embargoKeyError;
+	}
+
+	private final CharSequence getEmbargoJSON ( String t, String o ) {
+		try {
+			final Key key = getKeyForTime( t );
+
+			if ( o == null ) {
+				o = "";
+			}
+
+			// Wrap o -> embargo {t:123,o:"???",ts:{...}}"
+			final String embargoString = String.format( "{\"t\":%1$s,\"o\":\"%2$s\",\"ts\":%3$s}", t, o,
+				getSignedTimestampJSON( TS_MODE_EMBARGO ) );
+
+			final Cipher cipher = Cipher.getInstance( "AES/GCM/NoPadding" );
+			cipher.init( Cipher.ENCRYPT_MODE, key );
+
+			final byte[] encr = cipher.doFinal( embargoString.getBytes( StandardCharsets.UTF_8 ) );
+			final String encrBase64 = EncodeToB64( encr );
+
+			final byte[] ivBytes = cipher.getIV();
+			final String ivBase64 = EncodeToB64( ivBytes );
+
+			return String.format( "{\"t\":%1$s,\"iv\":\"%2$s\",\"ct\":\"%3$s\"}", t, ivBase64, encrBase64 );
+
+		} catch ( GeneralSecurityException e ) {
+			e.printStackTrace();
+		}
+
+		return embargoKeyError;
+	}
+
+	public void init ( ServletConfig config ) throws ServletException {
+
+		super.init( config );
+
+		loadConfig();
+
+		if ( keyPair == null ) {
+			logger.error( "FATAL: could not load or generate keys!" );
+		} else {
+			logger.info( "TBE initialized {}", getSignedTimestampJSON( TS_MODE_OTHER ) );
+		}
+	}
+
+	@Override
+	public void destroy () {
+
+		saveConfig();
+
+		super.destroy();
+	}
+
+	protected void doGet ( HttpServletRequest request, HttpServletResponse response )
+		throws ServletException, IOException {
+
+		// GET nothing -> SignedTimeStamp
+		final String tParm = request.getParameter( "t" );
+
+		if ( tParm == null ) {
+			response.getWriter().append( getSignedTimestampJSON( TS_MODE_QUERY ) );
+			return;
+		}
+
+		// GET t & t is in past -> Return key-for-t
+		if ( tParm.length() >= 13 ) {
+			try {
+
+				if ( Long.parseLong( tParm ) < System.currentTimeMillis() ) {
+					response.getWriter().append( getPastKeyJSON( tParm ) );
+					return;
+				}
+
+			} catch ( NumberFormatException nfe ) {
+				;
+			}
+		}
+
+		// * -> Return error
+		response.getWriter().append( signedTimestampError );
+
+	}
+
+	protected void doPost ( HttpServletRequest request, HttpServletResponse response )
+		throws ServletException, IOException {
+
+		final String tParm = request.getParameter( "t" );
+
+		// POST t & t is in future -> embargo "o" (which could be null)
+		if ( tParm != null && tParm.length() >= 13 ) {
+			try {
+
+				if ( Long.parseLong( tParm ) >= System.currentTimeMillis() ) {
+					response.getWriter().append( getEmbargoJSON( tParm, request.getParameter( "o" ) ) );
+					return;
+				}
+			} catch ( NumberFormatException nfe ) {
+				;
+			}
+
+		}
+
+		// * -> Return error
+		response.getWriter().append( signedTimestampError );
+
 	}
 
 }
