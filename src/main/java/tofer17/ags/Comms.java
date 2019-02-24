f46ef2e204329e245855494e79dfcfb3bee1d7f8
tofer17 <tofer17@gmail.com>
2019-02-24 10:01:01 -0600

    Primitive INSECURE & UNSAFE pass at a Comet client/server. BUGGY!!
    DO NOT USE IT AS IS

--- src/main/java/tofer17/ags/Comms.java
+++ src/main/java/tofer17/ags/Comms.java
@@ -1,6 +1,16 @@
 package tofer17.ags;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.AsyncEvent;
+import javax.servlet.AsyncListener;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,25 +19,182 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Comet-style reflector
+ *
+ * @author cmetyko
+ *
+ */
 public class Comms extends HttpServlet {
 
 	private static final long serialVersionUID = -2664038576539200092L;
 
-	@SuppressWarnings ( "unused" )
-	private static final Logger logger = LoggerFactory.getLogger( Comms.class );
+	private static final Logger logger = LoggerFactory.getLogger( TimeBasedEncrypter.class );
+
+	private static final Map<String,AsyncContext> waiters = new Hashtable<String,AsyncContext>();
+
+	private static final BlockingQueue<Envelope> messages = new LinkedBlockingQueue<Envelope>();
+
+	private Thread notifierThread = null;
 
 	public Comms () {
 		super();
 	}
 
+	private boolean pollQueue () {
+		try {
+
+			final Envelope envelope = messages.take();
+
+			logger.info( envelope.recipient + " " + envelope.message );
+
+			final AsyncContext ac = waiters.get( envelope.recipient );
+
+			logger.info( "{}", ac );
+
+			if ( ac != null ) {
+				try {
+					PrintWriter writer = ac.getResponse().getWriter();
+
+					writer.println(
+						String.format( "{\"r\":\"%1$s\",\"m\":\"%2$s\"}", envelope.recipient, envelope.message ) );
+					writer.flush();
+
+					ac.complete();
+				} catch ( IOException ioe ) {
+					logger.info( ioe.toString() );
+					waiters.remove( envelope.recipient );
+					messages.put( envelope );
+				}
+			} else {
+				messages.put( envelope );
+			}
+		} catch ( InterruptedException iex ) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public void init ( ServletConfig config ) throws ServletException {
+		super.init( config );
+
+		logger.info( "Comms init.." );
+		Runnable notifierRunnable = new Runnable() {
+
+			public void run () {
+				while ( pollQueue() ) {
+					;
+				}
+			}
+		};
+
+		notifierThread = new Thread( notifierRunnable );
+		notifierThread.start();
+
+	}
+
 	protected void doGet ( HttpServletRequest request, HttpServletResponse response )
 		throws ServletException, IOException {
-		response.getWriter().append( "Served at: " ).append( request.getContextPath() );
+		logger.info( "doGet..." );
+
+		response.setContentType( "text/html" );
+		response.setHeader( "Cache-Control", "private" );
+		response.setHeader( "Pragma", "no-cache" );
+
+		final String waiter = request.getParameter( "w" );
+
+		if ( waiter == null || "".equals( waiter ) ) {
+			response.sendError( 422, "nocando" );
+			return;
+		} else {
+			logger.info( "Establishing connection with '{}'...", waiter );
+		}
+
+		PrintWriter writer = response.getWriter();
+		// for IE
+		writer.println( "\n" );
+		writer.flush();
+
+		final AsyncContext ac = request.startAsync( request, response );
+		ac.setTimeout( 10 * 60 * 1000 );
+
+		waiters.put( waiter, ac );
+
+		ac.addListener( new AsyncListener() {
+
+			public void onComplete ( AsyncEvent event ) throws IOException {
+				waiters.remove( waiter );
+			}
+
+			public void onTimeout ( AsyncEvent event ) throws IOException {
+				waiters.remove( waiter );
+			}
+
+			public void onError ( AsyncEvent evt ) throws IOException {
+				;
+			}
+
+			public void onStartAsync ( AsyncEvent evt ) throws IOException {
+				;
+			}
+		} );
+
 	}
 
 	protected void doPost ( HttpServletRequest request, HttpServletResponse response )
 		throws ServletException, IOException {
-		doGet( request, response );
+		logger.info( "doPost..." );
+
+		response.setContentType( "text/plain" );
+		response.setHeader( "Cache-Control", "private" );
+		response.setHeader( "Pragma", "no-cache" );
+
+		request.setCharacterEncoding( "UTF-8" );
+
+		final String recipient = request.getParameter( "r" );
+		final String message = request.getParameter( "m" );
+
+		if ( recipient == null || "".equals( recipient ) ) {
+			response.sendError( 422, "nocando" );
+			return;
+		} else if ( message == null || "".equals( message ) ) {
+			response.sendError( 423, "nocando" );
+			return;
+		}
+
+		final Envelope envelope = new Envelope( recipient, message );
+
+		try {
+			messages.put( envelope );
+		} catch ( InterruptedException e ) {
+			e.printStackTrace();
+			response.sendError( 521, "sump'n went snap" );
+		}
+	}
+
+	@Override
+	public void destroy () {
+		logger.info( "Comms going dark bruh..." );
+
+		waiters.clear();
+		notifierThread.interrupt();
+
+		logger.info( "done" );
+		super.destroy();
+	}
+
+	private static final class Envelope {
+
+		public final String recipient;
+
+		public final String message;
+
+		public Envelope ( String recipient, String message ) {
+			this.recipient = recipient;
+			this.message = message;
+		}
+
 	}
 
 }
---
+++ src/test/webapp/Comms.js
@@ -0,0 +1,119 @@
+/**
+ *
+ */
+function setup () {
+	let el = document.getElementById( "hkupstatus" );
+	el.innerHTML = "Not connected";
+	el.style.color = "red";
+	el = document.getElementById( "wid" );
+	el.value = "tofer17-" + Math.round( Math.random() * 100 );
+}
+
+var abort = false;
+
+function hookup () {
+	let el = document.getElementById( "hkupstatus" );
+	el.innerHTML = "...connecting...";
+	el.style.color = "blue";
+	el = document.getElementById( "hkup" );
+	el.innerHTML = "...abort..";
+
+	if ( xhr ) {
+		abort = true;
+		xhr.abort();
+		el.innerHTML = "Connect";
+		el = document.getElementById( "hkupstatus" );
+		el.innerHTML = "Not connected";
+		el.style.color = "red";
+		xhr = null;
+	} else {
+		connect();
+	}
+}
+
+var xhr, wid;
+
+function connect () {
+	wid = document.getElementById( "wid" ).value;
+	xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = conn;
+	xhr.open( "GET", "com?w=" + wid );
+	xhr.onload = connected;
+	xhr.send();
+}
+
+function conn () {
+	//console.log( this );
+	let el = document.getElementById( "hkupstatus" );
+	switch ( this.readyState ) {
+		case XMLHttpRequest.UNSENT : // 0
+			break;
+		case XMLHttpRequest.OPENED : // 1
+			el.innerHTML = "opened";
+			console.log( "Opened" );
+			break;
+		case XMLHttpRequest.HEADERS_RECEIVED : // 2
+			break;
+		case XMLHttpRequest.LOADING : // 3
+			el.innerHTML = "Connected";
+			el.style.color = "green";
+			el = document.getElementById( "hkup" );
+			el.innerHTML = "Disconnect";
+			el.disabled = false;
+			break;
+		case XMLHttpRequest.DONE : // 4
+			console.error( "DONE" );
+			if ( !abort ) {
+				displayMessage( JSON.parse( this.response ) );
+				connect();
+			} else {
+				abort = false;
+			}
+			break;
+
+	}
+}
+
+function displayMessage ( msg ) {
+	let recip = document.createElement( "span" );
+	recip.className = "recip";
+	recip.innerHTML = msg.r;
+
+	let message = document.createElement( "span" );
+	message.className = "msgbody";
+	message.innerHTML = msg.m;
+
+	let div = document.createElement( "div" );
+	div.className = "message";
+	div.appendChild( recip );
+	div.appendChild( message );
+
+	document.getElementById( "recemsg" ).appendChild( div );
+
+}
+
+function connected () {
+	console.error( this );
+}
+
+function sendMsg () {
+	let el = document.getElementById( "sendmsg" );
+	let post = new XMLHttpRequest();
+	post.open( "POST", "com" );
+	post.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	//post.onload = connected;
+	post.send(
+		"r=" + wid +
+		"&m=" + el.value
+	);
+}
+
+/** ************************************************** */
+window.addEventListener( "load", () => {
+	setup();
+});
+
+function hideKids ( evt ) {
+	const n = evt.target.nextElementSibling;
+	n.style.display = n.style.display == "" ? "none" : "";
+}
